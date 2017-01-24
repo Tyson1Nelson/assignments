8fab3073620fe767ff245a010ebb94ac0e816fe2
Tyson Nelson <13tyson13@gmail.com>
2017-01-24 08:51:47 -0700

    updated business time project and colossal rpg project

--- projects/colossalAdvRpg/rpg.js
+++ projects/colossalAdvRpg/rpg.js
@@ -1,12 +1,12 @@
 var readline = require('readline-sync');
-//var userName = readline.question('May I have your name? ');
-//console.log('Hi ' + userName + '!');
-//var startAdv = readline.keyInYN('Are you ready to start your Adventure?');
-//if (startAdv === true) {
-//    console.log("Lets get started!");
-//} else {
-//    console.log("come back when you are ready");
-//}
+var userName = readline.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
+var startAdv = readline.keyInYN('Are you ready to start your Adventure?');
+if (startAdv === true) {
+    console.log("Lets get started!");
+} else {
+    console.log("come back when you are ready");
+}
 var options = ["Walk", "Look", "Check Inventory"];
 
 var playerHealth = 100;
@@ -26,7 +26,7 @@ while (playerHealth > 0) {
 }
 
 function walk() {
-    if (randomNum(0, 3) === 0) {
+    if (randomNum(1, 3) === 1) {
         enemiesAttack();
     } else {
         console.log("So far you are safe on your journey!")
@@ -42,6 +42,7 @@ function look() {
 }
 
 function inventory(itemToAdd) {
+    console.log("your health is " + playerHealth);
     if (itemToAdd) {
         usersItems.push(itemToAdd);
     }
@@ -55,41 +56,57 @@ function randomNum(min, max) {
 
 function enemiesAttack() {
     var enemyEncountered = enemies[randomNum(0, 2)]
-    var enemyHealth = randomNum(1, 50);
+    var enemyHealth = randomNum(25, 80);
     attackOrRun(enemyEncountered, enemyHealth);
 }
 
 function attackOrRun(enemy, health) {
-    var attkOrRun = readline.keyInYN("you're currently " + enemy + " proceed attacking?!")
+    var attkOrRun = readline.keyInYN("you're currently facing a " + enemy + " with " + health + " health points, proceed with attacking?!")
     if (attkOrRun === true) {
         fighting(enemy, health);
     } else {
-        escape();
+        escape(enemy, health);
     }
 }
 
 function fighting(enemyEncountered, enemyHealth) {
-    var attack = randomNum(1, 70);
-    console.log("You've done " + attack + " points of damage");
+    var attack = randomNum(1, 40);
+    var enemyAttacking = randomNum(1, 70);
     enemyHealth = enemyHealth - attack;
-    console.log(enemyHealth);
-    if (enemyHealth < 0) {
+    playerHealth = playerHealth - enemyAttacking;
+    console.log("You've done " + attack + " points of damage");
+    console.log(enemyEncountered + " has " + enemyHealth + " hp");
+
+    if (enemyHealth > 0 && playerHealth > 0) {
+        console.log("enemy has done " + enemyAttacking + " points of damage");
+        console.log("you currently have " + playerHealth + " points");
+        attackOrRun(enemyEncountered, enemyHealth);
+
+    } else if (enemyHealth <= 0) {
+        console.log("You have defeated " + enemyEncountered);
         var randomItem = items[randomNum(0, 3)];
         inventory(randomItem);
-    } else {
+        playerHealth = playerHealth + 10;
+        console.log("You now have " + playerHealth + " hp");
 
+    } else if (playerHealth <= 0) {
+        console.log("enemy has done " + enemyAttacking + " points of damage");
+        console.log("you currently have " + playerHealth + " points");
+        console.log("Game Over")
+    } else {
+        attackOrRun(enemyEncountered, enemyHealth);
     }
 }
 
-function escape() {
+function escape(enemy, health) {
     if (randomNum(1, 2) === 1) {
         console.log("You've escaped succesfully");
     } else {
         console.log("you are unable to escape ");
+        fighting(enemy, health);
     }
 }
 
 function attack() {
-    randomNum(1, 70);
+   
 }
-enemies[randomNum(0, 2)]
